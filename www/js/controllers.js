82b13b9bb57846d61934f70a9fee093997953916
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-06-23 17:18:20 +0530

    Created service framework

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,17 +14,47 @@ angular.module('perkkx.controllers', [])
     }
 })
 
-.controller('PendingCtrl', function($scope) {
+.controller('PendingCtrl', function($scope, pxApiConnect) {
     $scope.pcodes = []
+    pxApiConnect.setCallback('pending', function(data) {
+        $scope.$apply(function() {
+            $scope.pcodes = data;
+        });
+    });
+    pxApiConnect.apiGet('pending')
+
     $scope.submit = function(obj) {};
 })
 
 .controller('UsedCtrl', function($scope){
     $scope.ucodes = [];
+    pxApiConnect.setCallback('used', function(data) {
+        $scope.$apply(function() {
+            $scope.ucodes = data;
+        });
+    });
+    pxApiConnect.apiGet('used')
     $scope.submit = function(obj) {};
 })
 
 .controller('ExpiredCtrl', function($scope){
     $scope.ecodes = [];
+    pxApiConnect.setCallback('expired', function(data) {
+        $scope.$apply(function() {
+            $scope.ecodes = data;
+        });
+    });
+    pxApiConnect.apiGet('expired')
+    $scope.submit = function(obj) {};
+})
+
+.controller('DisputeCtrl', function($scope) {
+    $scope.dcodes = [];
+    pxApiConnect.setCallback('disputed', function(data) {
+        $scope.$apply(function() {
+            $scope.dcodes = data;
+        });
+    });
+    pxApiConnect.apiGet('disputed')
     $scope.submit = function(obj) {};
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,33 +1,36 @@
 angular.module('perkkx.services', [])
-
-.factory('Chats', function($http,$log) {
-	var chats=[];
-  $http.get("http://jlabs.co/perkkx/code.php",{cache:true})
-	.success(function(result){
-		angular.copy(result,chats)
-		console.log(chats);
-	});
-   
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
+.factory('pxApiConnect', function($http, $log) {
+    var vendor_id = 1;
+    var urls = {            // TODO
+        pending: "todo",
+        used: "todo",
+        expired: "todo",
+        disputed: "todo"
     }
-  };
-})
+
+    /* Currently, not employing the error callback */
+    var callbacks = {
+        pending: function(data) {},
+        used: function(data) {},
+        expired: function(data) {},
+        disputed: function(data) {}
+    }
+
+    var res = {
+        setCallback: function(key, receiver) {
+            callbacks[key] = receiver;
+        },
+        apiGet: function(key) {
+            // Use $http service to call the api
+            $http.jsonp(urls[key])
+            .success( function(data) { callbacks[key](data) })
+        }
+    }
+        
+});
 
 
+/*
 .factory('usedcode', function($http,$log) {
 	var ucodes=[];
   $http.get("http://45.55.72.208/perkkx/merchant/coupon/1",{cache:true})
@@ -53,4 +56,4 @@ angular.module('perkkx.services', [])
       return null;
     }
   };
-});
+}); */
